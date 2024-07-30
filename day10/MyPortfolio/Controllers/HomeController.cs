5e368e78173345709f155085fa1200686ae6cb1b
guswlrla <rlaguswl_01@naver.com>
2024-07-30 17:19:50 +0900

    리드미 정리...

--- day10/MyPortfolio/Controllers/HomeController.cs
+++ day10/MyPortfolio/Controllers/HomeController.cs
@@ -1,9 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MyPortfolio.Data;
 using MyPortfolio.Helper;
 using MyPortfolio.Models;
 using System.Diagnostics;
-using System.Security.Cryptography; // 암호화 
+using System.Security.Cryptography; // 암호화
 
 namespace MyPortfolio.Controllers
 {
@@ -12,10 +13,10 @@ namespace MyPortfolio.Controllers
         //private readonly ILogger<HomeController> _logger;
         private readonly AppDbContext _context;
 
-        //public HomeController(ILogger<HomeController> logger, AppDbContext _context)
+        //public HomeController(ILogger<HomeController> logger, AppDbContext context)
         //{
         //    _logger = logger;
-        //    AppDbContext context; // DB를 연결해서 사용하겠다는 초기화 설정
+        //    _context = context; // DB를 연결해서 사용하겠다는 초기화
         //}
 
         public HomeController(AppDbContext context)
@@ -23,8 +24,27 @@ namespace MyPortfolio.Controllers
             _context = context;
         }
 
-        [HttpGet] // 디폴트 값이라 생략가능하지만 지금은 작성 
+        [HttpGet] // Default로 생략가능
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        // 회원로그인으로 새로 추가
+        [HttpGet]
+        public IActionResult Login()
         {
             return View();
         }
@@ -32,14 +52,14 @@ namespace MyPortfolio.Controllers
         [HttpPost]
         public IActionResult Login(User user)
         {
-            // userEmail, password md5로 암호화
+            // userEmail, password md5로 암호화 
             // DB에 있는 값과 비교
             var mdHash = MD5.Create();
             user.Password = Common.GetMd5Hash(mdHash, user.Password); // 로그인창에 입력한 암호를 암호화
 
             var result = _context.User.FirstOrDefault(u => u.UserEmail == user.UserEmail && u.Password == user.Password);
 
-            if (result == null) // 로그인 할 사람이 없다
+            if (result == null) // 로그인할 사람이 없다
             {
                 return View(user);
             }
@@ -57,7 +77,7 @@ namespace MyPortfolio.Controllers
         [HttpGet]
         public IActionResult Logout()
         {
-            // 로그인시 생선된 세션 삭제
+            // 로그인시 생성된 세션 삭제
             HttpContext.Session.Remove("USER_LOGIN_KEY");
             HttpContext.Session.Remove("USER_NAME");
             HttpContext.Session.Remove("USER_EMAIL");
@@ -65,54 +85,54 @@ namespace MyPortfolio.Controllers
             return RedirectToAction("Index", "Home");
         }
 
-        [HttpGet]
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-
-        // 회원로그인으로 새로추가 
-        [HttpGet]
-        public IActionResult Login()
-        {
-            return View();
-        }
-
-        // 회원등록 페이지를 열기 
+        // 회원등록 페이지를 열어줘
         [HttpGet]
         public IActionResult Register()
         {
             return View();
         }
-        // 회원등록 페이지 내용을 DB에 저장 
+
+        // 회워등록 페이지 내용을 DB에 저장해줘.
         [HttpPost]
         public async Task<IActionResult> Register(User user)
         {
+            // 패스워드 두개가 일치하지 않을때 일부러 검증에러를 발생!
             if (user.Password != user.PasswordCheck)
             {
-                // 패스워드 두 개가 일치하지 않을 때, 일부러 검증에러를 발생시킴 
-                ModelState.AddModelError("passwordCheck", "패스워드가 일치하지 않습니다.");
+                ModelState.AddModelError("PasswordCheck", "패스워드가 일치하지 않습니다.");
             }
 
             if (ModelState.IsValid)
             {
-                user.RegDate = DateTime.Now; // 회원 등록 날짜를 오늘로 저장
+                // 모든값이 정확히 들어왔음
+                user.RegDate = DateTime.Now; // 회원 등록날짜 오늘로 지정
 
                 var mdHash = MD5.Create();
                 user.Password = Common.GetMd5Hash(mdHash, user.Password);
                 user.PasswordCheck = null;
 
-                _context.Add(user); // Insert
-                await _context.SaveChangesAsync();  // Commit 
+                _context.Add(user); // INSERT
+                await _context.SaveChangesAsync(); // COMMIT
                 return RedirectToAction("Login");
             }
+
             return View(user);
+        }
+
+        public IActionResult Project()
+        {
+            // DB Project 테이블 내용을 리스트로 받아서 View로 전달
+            var list = _context.Project.ToList();
+
+            foreach (var item in list)
+            {
+                item.FilePath = item.FilePath.Replace("\\", "/");
+                var index = item.FilePath.IndexOf("uploads");
+                var finalPath = item.FilePath.Substring(index - 1);
+                item.FilePath = finalPath;
+            }
+
+            return View(list);
         }
     }
 }
--- day10/MyPortfolio/Models/Board.cs
+++ day10/MyPortfolio/Models/Board.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace MyPortfolio.Models
@@ -8,37 +9,40 @@ namespace MyPortfolio.Models
         [Key]
         public int Id { get; set; } // PK
 
-        [Required(ErrorMessage="이름을 입력해주세요.")]
-        [MaxLength(50)] // NVARCHAR(50) 사이즈 지정을 하려면
-        [DisplayName("이름")]
-        public string Name { get; set; } // 작성자명
+        //[Required(ErrorMessage ="이름은 필수입니다")]
+        //[MaxLength(50)]  // NVARCHAR(50) 사이즈 지정하려면
+        //[DisplayName("이름")]
+        //public string Name { get; set; } // 작성자명
 
-        [Required(ErrorMessage = "아이디를 입력해주세요.")]
-        [MaxLength(20)]
-        [DisplayName("아이디")]
-        public string UserId{ get; set; } // 작성자 아이디
+        //[Required(ErrorMessage = "아이디는 필수입니다")]
+        //[MaxLength(20)]
+        //[DisplayName("아이디")]
+        //public string UserId { get; set; } // 작성자 아이디
 
-        [Required(ErrorMessage = "제목을 입력해주세요.")]
+        [Required(ErrorMessage = "제목은 필수입니다")]
         [MaxLength(250)]
         [DisplayName("제목")]
-        public string Title{ get; set; } // 게시글 제목
+        public string Title { get; set; } // 게시글 제목
 
-        [Required(ErrorMessage = "내용을 입력해주세요.")]
+        [Required(ErrorMessage = "내용은 필수입니다")]
         [DisplayName("내용")]
-        public string Contents{ get; set; } // 게시글 내용
+        public string Contents { get; set; } // 게시글 내용
 
-        // ? : DB에서 null 허용
+        // ? DB에서 Null 허용
         [DisplayName("조회수")]
-        public int? Hit{ get; set; } // 읽은 횟수
+        public int? Hit { get; set; } // 읽은 횟수
 
         [DisplayName("작성일")]
-        public DateTime? RegDate { get; set; } = DateTime.Now; // 게시글 작성일자
+        public DateTime? RegDate { get; set; } // 게시글 작성일자
 
         // Nullable 변수로 변경
         [DisplayName("수정일")]
-        public DateTime? ModDate{ get; set; } // 게시글 최종 수정일자
+        public DateTime? ModDate { get; set; } // 게시글 최종 수정일자
 
-        // RelatrionShip User부모 -> Board자식
-        public virtual User User { get; set; }
+        // RelationShip 부모User->자식Board
+        public virtual User? User { get; set; }
+
+        [DisplayName("작성자명")]
+        public virtual string? UserName { get; set; }
     }
 }
